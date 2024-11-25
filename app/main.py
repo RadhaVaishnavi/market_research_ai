e07ba3331b6359bb712cf012bc5f6b9f4c53ae44
RadhaVaishnavi <84319477+RadhaVaishnavi@users.noreply.github.com>
2024-11-25 19:38:13 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import os
 
 print("Python Paths:", sys.path)  # Debugging: Check available paths
 print("Current Directory:", os.getcwd())  # Debugging: Check where the app runs from
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 import streamlit as st
